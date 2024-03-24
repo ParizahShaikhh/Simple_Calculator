2ea811f3be33a1137f92494da434be844dd9f81e
Parizah Shaikh <parivian6@gmail.com>
2024-03-25 01:22:03 +0500

    FIRST SIMPLE_CALCULATOR PROJECT.

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+// shabang
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    { message: "Enter Your First Number",
+    type: "number",
+    name: "FirstNumber"},
+    { message: "Enter Your Second Number",
+    type: "number",
+    name: "SecondNumber"},
+    {
+        message: "Selected Operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+
+// Conditional Statements 
+if (answer.operator === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+} else if (answer.operator === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+} else if (answer.operator === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+} else if (answer.operator === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+} else {
+    console.log("Please Select Another Valid Operator");
+} 
+    
